e66575645259efd9ed94b34af24b6065a1839cd5
Dat Proto <dotatdat1997@hotmail.com>
2023-03-21 15:15:01 +0700

    INITIAL SETUP: App Style basic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <p className="text-5xl font-bold">Weight</p>
+      <h1>Weight</h1>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,27 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['var(--font-jost)']
+      },
+      colors: {
+        prod: {
+          purple: '#AD1FEA',
+          blue: {
+            alice: '#F2F4FF',
+            american: {
+              dark: '#373F68',
+              main: '#3A4374'
+            },
+            gray: {
+              dark: '#647196'
+            },
+            maya: '#62BCFA',
+            royal: '#4661E6'
+          },
+          tangerine: '#F49F85',
+          white: {
+            ghost: '#F7F8FD'
+          }
+        }
       }
     },
   },
